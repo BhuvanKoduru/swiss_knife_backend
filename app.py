d56f102fa6d4a0c3ae292a9e9842eae1d4d2ae27
Bhuvan <83451456+BhuvanKoduru@users.noreply.github.com>
2023-03-21 18:12:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ model = AutoModelForSequenceClassification.from_pretrained("codeparrot/unixcoder
 
 import os
 import string
+
+def predict_complexity(code):
+  inputs = tokenizer(code, return_tensors="pt")
+
+  op = model(**inputs)
+
+  values = []
+  names=['constant', 'cubic', 'linear', 'logn', 'nlogn', 'np', 'quadratic']
+  for val in op.logits[0]:
+    values.append(val.item())
+
+  return names[(values.index(max(values)))]
