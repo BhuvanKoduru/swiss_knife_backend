78819a9e23563c2a5a037b5b7f1f7a993f110689
Bhuvan <83451456+BhuvanKoduru@users.noreply.github.com>
2023-03-21 19:00:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, request, send_file
 
+import os
+import string
+
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 tokenizer = AutoTokenizer.from_pretrained("codeparrot/unixcoder-java-complexity-prediction")
 model = AutoModelForSequenceClassification.from_pretrained("codeparrot/unixcoder-java-complexity-prediction")
@@ -16,6 +19,37 @@ def predict_complexity(code):
 
   return names[(values.index(max(values)))]
 
+def get_code_maintainability(filepath):
+    def get_maintainability_index(report):
+        symbols = set()
+
+        alphanum = (list(string.ascii_lowercase + string.ascii_uppercase + string.digits +  '()+-.─' + '\n' + ' '))
+
+        for i in report:
+            if i not in alphanum:
+                symbols.add(i)
+
+        for sym in symbols:
+            report = report.replace(sym, " ")
+
+        return float(report.split()[19])
+    
+    filename = filepath.split('/')[-1]
+    # folder = '/'.join(filepath.split('/')[:-1]).strip()
+    folder = '/'.join(filepath.split('/')[:-1]).strip()
+    folder = folder.replace(r"\ ", " ")
+    
+    os.chdir(folder)
+    os.system("ls")
+    os.system("wily build")
+    os.system("wily report " + filename + " > report.txt")
+
+    f = open("report.txt", "r")
+    text = (f.readlines())
+    text = ''.join(text) 
+
+    return get_maintainability_index(text)
+
 app = Flask(__name__)
   
 # From React, send GET request to localhost:5000/some_code
@@ -26,6 +60,13 @@ def pred():
     output = predict_complexity(input)
     # {'output' :  'linear'}
     return jsonify({'output': output})
+ 
+
+@app.route('/maintainability', methods = ['POST'])
+def maintain():
+    urlPath = request.args.get('path')
+    output = get_code_maintainability(urlPath)
+    return jsonify({'output': output})
   
- __name__ == '__main__':
+ if __name__ == '__main__':
     app.run(debug = True)
